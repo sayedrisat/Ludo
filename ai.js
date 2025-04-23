b6dd9de08ef8f174f6d63b035c24b43ee268cf7d
Sayed Risat <sayedrisat.dm@gmail.com>
2025-04-23 20:58:10 +0600

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,43 +1,38 @@
 function aiMove(player, validTokens) {
   let chosenToken = null;
 
-  // Prioritize cutting opponent's token
-  validTokens.forEach(token => {
-    let newIndex;
-    if (boardState[player.color].home.includes(token)) {
-      if (diceResult === 6) {
-        newIndex = 0;
-      }
-    } else {
+  // If can unlock, prioritize unlocking
+  if (boardState[player.color].locked.length > 0 && moveQueue[0] === 6) {
+    chosenToken = validTokens[0];
+  } else if (boardState[player.color].locked.length === 0) {
+    // Prioritize cutting opponent's token
+    validTokens.forEach(token => {
+      let newIndex;
       const pathToken = boardState[player.color].path.find(t => t.token === token);
       const homePathToken = boardState[player.color].homePath.find(t => t.token === token);
       if (pathToken) {
-        newIndex = pathToken.index + diceResult;
+        newIndex = pathToken.index + moveQueue[0];
       }
-    }
-    if (newIndex !== undefined && newIndex < 51 && !safeZones.includes(newIndex)) {
-      players.forEach(p => {
-        if (p.color !== player.color) {
-          if (boardState[p.color].path.some(t => t.index === newIndex)) {
-            chosenToken = token;
+      if (newIndex !== undefined && newIndex < 51 && !safeZones.includes(newIndex)) {
+        players.forEach(p => {
+          if (p.color !== player.color) {
+            if (boardState[p.color].path.some(t => t.index === newIndex)) {
+              chosenToken = token;
+            }
           }
-        }
-      });
-    }
-  });
+        });
+      }
+    });
 
-  // If no cut possible, move furthest token or closest to home
-  if (!chosenToken) {
-    let maxIndex = -1;
-    let closestToHome = 6;
-    validTokens.forEach(token => {
-      if (boardState[player.color].home.includes(token)) {
-        if (diceResult === 6) chosenToken = token;
-      } else {
+    // If no cut possible, move furthest token or closest to home
+    if (!chosenToken) {
+      let maxIndex = -1;
+      let closestToHome = 6;
+      validTokens.forEach(token => {
         const pathToken = boardState[player.color].path.find(t => t.token === token);
         const homePathToken = boardState[player.color].homePath.find(t => t.token === token);
         if (pathToken) {
-          const newIndex = pathToken.index + diceResult;
+          const newIndex = pathToken.index + moveQueue[0];
           if (newIndex < 51) {
             if (pathToken.index > maxIndex) {
               maxIndex = pathToken.index;
@@ -47,14 +42,14 @@ function aiMove(player, validTokens) {
             chosenToken = token;
           }
         } else if (homePathToken) {
-          const newIndex = homePathToken.index + diceResult;
+          const newIndex = homePathToken.index + moveQueue[0];
           if (newIndex <= 5 && (5 - newIndex) < closestToHome) {
             closestToHome = 5 - newIndex;
             chosenToken = token;
           }
         }
-      }
-    });
+      });
+    }
   }
 
   if (chosenToken) {
