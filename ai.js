319f723224f1f53e83f1bdc8b55c304ebfc2ab0e
Sayed Risat <sayedrisat.dm@gmail.com>
2025-04-23 20:20:38 +0600

    Update ai.js

--- ai.js
+++ ai.js
@@ -6,16 +6,19 @@ function aiMove(player, validTokens) {
     let newIndex;
     if (boardState[player.color].home.includes(token)) {
       if (diceResult === 6) {
-        newIndex = getStartIndex(player.color);
+        newIndex = 0;
       }
     } else {
       const pathToken = boardState[player.color].path.find(t => t.token === token);
-      newIndex = pathToken.index + diceResult;
+      const homePathToken = boardState[player.color].homePath.find(t => t.token === token);
+      if (pathToken) {
+        newIndex = pathToken.index + diceResult;
+      }
     }
-    if (newIndex !== undefined && !safeZones.includes(newIndex)) {
+    if (newIndex !== undefined && newIndex < 51 && !safeZones.includes(newIndex)) {
       players.forEach(p => {
         if (p.color !== player.color) {
-          if (p.path.some(t => t.index === newIndex)) {
+          if (boardState[p.color].path.some(t => t.index === newIndex)) {
             chosenToken = token;
           }
         }
@@ -23,17 +26,32 @@ function aiMove(player, validTokens) {
     }
   });
 
-  // If no cut possible, move furthest token
+  // If no cut possible, move furthest token or closest to home
   if (!chosenToken) {
     let maxIndex = -1;
+    let closestToHome = 6;
     validTokens.forEach(token => {
       if (boardState[player.color].home.includes(token)) {
         if (diceResult === 6) chosenToken = token;
       } else {
         const pathToken = boardState[player.color].path.find(t => t.token === token);
-        if (pathToken.index > max  maxIndex) {
-          maxIndex = pathToken.index;
-          chosenToken = token;
+        const homePathToken = boardState[player.color].homePath.find(t => t.token === token);
+        if (pathToken) {
+          const newIndex = pathToken.index + diceResult;
+          if (newIndex < 51) {
+            if (pathToken.index > maxIndex) {
+              maxIndex = pathToken.index;
+              chosenToken = token;
+            }
+          } else if (newIndex <= 56) {
+            chosenToken = token;
+          }
+        } else if (homePathToken) {
+          const newIndex = homePathToken.index + diceResult;
+          if (newIndex <= 5 && (5 - newIndex) < closestToHome) {
+            closestToHome = 5 - newIndex;
+            chosenToken = token;
+          }
         }
       }
     });
@@ -42,6 +60,6 @@ function aiMove(player, validTokens) {
   if (chosenToken) {
     moveToken(player, chosenToken);
   } else {
-    nextTurn();
+    setTimeout(nextTurn, 500);
   }
 }
